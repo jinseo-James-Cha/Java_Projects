0d2aa6c59e5488740b5ce10281abb2053c68abd4
James <up1912@gmail.com>
2021-03-10 23:55:11 +0900

    2021-03-10 TempoInBPM과

--- beatbox/src/com/james/beatbox/MiniMusicPlayer2.java
+++ beatbox/src/com/james/beatbox/MiniMusicPlayer2.java
@@ -14,15 +14,54 @@ public class MiniMusicPlayer2 implements ControllerEventListener{
 
 	public void go() {
 	
-		
+		try {
+			Sequencer sequencer = MidiSystem.getSequencer();
+			sequencer.open();
+			
+			// 이벤트 시퀀서에 등록
+			// ControllerEvent 목록을 나타내는 int[]을 두번째 인자로 요구
+			int[] eventsIWant = {127};
+			sequencer.addControllerEventListener(this, eventsIWant);
+			
+			Sequence seq = new Sequence(Sequence.PPQ, 4);
+			Track track = seq.createTrack();
+			
+			for( int i = 40; i < 100; i += 4) {
+				track.add(makeEvent(144,1,i,100,i));
+				track.add(makeEvent(176, 1, 127, 0, i));
+				track.add(makeEvent(128,1,i,100,i+2));
+			}
+			
+			sequencer.setSequence(seq);
+			sequencer.setTempoInBPM(220);
+			sequencer.start();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
-	
+		
 	@Override
 	public void controlChange(ShortMessage event) {
-		// TODO Auto-generated method stub
-		
+		System.out.println("la");
 	}
 	
+	
+	/*
+	 * 음이 증가하여 여러 음표를 Track에 담고 실행하기 위해 method로 구현 
+	 * */
+	public MidiEvent makeEvent(int comd, int chan, int one, int two, int tick) {
+		MidiEvent event = null;
+		
+		try {
+			ShortMessage a = new ShortMessage();
+			a.setMessage(comd, chan, one, two);
+			event = new MidiEvent(a, tick);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return event;
+	}
 	
 
 }
